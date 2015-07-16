0bc0de4eabd956d475342325bd52007c07f97f6a
dmoros <danielousky@gmail.com>
2015-07-16 05:09:50 -0430

    Avances en las funcionalidades

--- app/controllers/cal_principal_estudiante_controller.rb
+++ app/controllers/cal_principal_estudiante_controller.rb
@@ -3,7 +3,9 @@ class CalPrincipalEstudianteController < ApplicationController
 	before_filter :cal_filtro_logueado
 	before_filter :cal_filtro_estudiante
 
-	def index		
+	def index
+		@estudiante = session[:cal_usuario].cal_estudiante
+
 	end
 
 end
