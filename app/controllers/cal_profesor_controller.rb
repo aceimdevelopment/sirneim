2db7e0aa25eab58e182e7accf75a12f60935c8df
dmoros <danielousky@gmail.com>
2015-08-03 20:20:31 -0430

    Agregada funcionalidad para el registro de Porfesores

---
+++ app/controllers/cal_profesor_controller.rb
@@ -0,0 +1,26 @@
+class CalProfesorController < ApplicationController
+
+	def nuevo
+		# @cal_profesor = CalProfesor.new
+	end
+
+	def crear
+		@cal_usuario = CalUsuario.new(params[:cal_usuario])
+
+		if @cal_usuario.save
+			params[:cal_profesor][:cal_usuario_ci] = @cal_usuario.ci
+			@cal_profesor = CalProfesor.new(params[:cal_profesor])
+			if @cal_profesor.save
+				flash[:success] = "Profesor Registrado satisfactoriamente"
+				redirect_to :controller => 'cal_principal_admin', :action => 'usuarios'
+			else
+				flash[:error] = "No se pudo registrar el profesor, revisa lo siguiente: #{@cal_profesor.errors.message.join(' ')}"
+				render :action => 'nuevo'
+			end
+		else
+			flash[:error] = "No se pudo registrar el usuario, revisa lo siguiente: #{@cal_usuario.errors.message.join(' ')}"
+			render :action => 'nuevo'
+		end	
+	end
+
+end
