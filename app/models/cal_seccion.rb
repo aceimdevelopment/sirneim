2fe2a47215323a49e0f5bfcb921393730dde0f92
dmoros <danielousky@gmail.com>
2016-01-05 23:25:32 -0430

    Ajuste de secciones: solo se muestran las secciones del periodo altual (2h). Depuracion: modificacion de cal_seccion ajuste CalEstudianteSeccion (1h)

--- app/models/cal_seccion.rb
+++ app/models/cal_seccion.rb
@@ -13,8 +13,9 @@ class CalSeccion < ActiveRecord::Base
 	has_many :cal_estudiantes_secciones,
 		:class_name => 'CalEstudianteSeccion',
  		:foreign_key => [:cal_numero, :cal_materia_id, :cal_semestre_id],
- 		:primary_key => [:cal_numero, :cal_materia_id, :cal_semestre_id]
-
+ 		# :primary_key => [:cal_numero, :cal_materia_id, :cal_semestre_id]
+ 		# ojo al colocar la linea anterior dejan de aparecer los estudiantes_secciones de las secciones
+ 		:primary_key => [:numero, :cal_materia_id, :cal_semestre_id]
 	accepts_nested_attributes_for :cal_estudiantes_secciones
 
 	has_many :cal_estudiantes, :through => :cal_estudiantes_secciones, :source => :cal_estudiante  
