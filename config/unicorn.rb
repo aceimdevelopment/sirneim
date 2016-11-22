86c8a1afe685704def6c2d7f354998d095869209
danielmonetizadores <daniel@monetizadores.com>
2016-11-22 12:28:24 -0400

    Ajuste de Unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,36 @@
-worker_processes 3
-preload_app true
-timeout 30
-listen 5001
+#worker_processes 3
+#preload_app true
+#timeout 30
+#listen 5001
 #rails_root  = `pwd`.gsub("\n", "")
-rails_root = "/home/aceimdiplomados/aceim_diplomados"
-pid         "#{rails_root}/tmp/pids/unicorn.pid"
+#rails_root = "/home/aceimdiplomados/aceim_diplomados"
+#pid         "#{rails_root}/tmp/pids/unicorn.pid"
 #stderr_path "#{rails_root}/log/unicorn.log"
 #stdout_path "#{rails_root}/log/unicorn.log"
 
-after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-end
+#after_fork do |server, worker|
+#  ActiveRecord::Base.establish_connection
+#end
+
+
+
+# set path to application
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+working_directory app_dir
+
+
+# Set unicorn options
+worker_processes 2
+preload_app true
+timeout 30
+
+# Set up socket location
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+
+# Logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "#{shared_dir}/pids/unicorn.pid"
