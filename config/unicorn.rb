6dae434e31e3bea9fb736fdd460d4087dd8d1887
danielmonetizadores <daniel@monetizadores.com>
2016-11-24 01:20:09 -0400

    Ajustes de unicorn y gemfile para ruby 1.8.7

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,18 +15,19 @@
 
 
 # set path to application
-app_dir = File.expand_path("../..", __FILE__)
+# app_dir = File.expand_path("../..", __FILE__)
+app_dir = "/home/aceimdiplomados/aceim_diplomados"
 shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 
 # Set unicorn options
-worker_processes 2
+worker_processes 3
 preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen 5001 #"#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
@@ -34,3 +35,8 @@ stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
 pid "#{shared_dir}/pids/unicorn.pid"
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
+
