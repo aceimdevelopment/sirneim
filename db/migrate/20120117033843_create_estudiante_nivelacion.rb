6478339b1eb7957dc2dd053f4224cb76966c5ff2
Unknown <aceim.development@gmail.com>
2018-09-27 17:35:16 -0400

    Limpieza – Migraciones 03:
    Se procedió a eliminar migraciones obsoletas y a limpiar el archivo schema.rb. (No incluye tablas en la base de datos).

--- db/migrate/20120117033843_create_estudiante_nivelacion.rb
+++
@@ -1,8 +0,0 @@
-class CreateEstudianteNivelacion < ActiveRecord::Migration
-  def change
-    create_table :estudiante_nivelacion do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140426022859_create_docente.rb
+++
@@ -1,8 +0,0 @@
-class CreateDocente < ActiveRecord::Migration
-  def change
-    create_table :docente do |t|
-
-      t.timestamps
-    end
-  end
-end
